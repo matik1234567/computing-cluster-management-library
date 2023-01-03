bfcad0a417b868108e81d6b593522c470c100f6c
Mateusz Machura <machurka123@gmail.com>
2023-01-03 15:20:14 +0100

    1.0.3

--- Examples/AvgOfArray/AvgOfArray/Program.cs
+++ Examples/AvgOfArray/AvgOfArray/Program.cs
@@ -4,11 +4,50 @@ namespace AvgOfArray
 {
     class Program
     {
+        public static void ServerMode()
+        {
+            Dictionary<string, dynamic> map = new Dictionary<string, dynamic>();
+            map.Add("heartBeatFrequency", 7);
+            map.Add("listCount", 100000);
+            GlobalVariables globalVariables = new GlobalVariables(map);
+            ServerRuntime serverRuntime = new ServerRuntime(globalVariables);
+
+            serverRuntime.Server(1302);
+            _ = serverRuntime.RunServerAsync();
+            serverRuntime.Invite();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            serverRuntime.RunTask();
+        }
+
+        public static void ClientMode()
+        {
+            ClientRuntime clientRuntime = new ClientRuntime();
+            clientRuntime.Client("192.168.1.139", 1302);
+            clientRuntime.EnrollToProject();
+            clientRuntime.RunClient();
+        }
+
         public static void Main(string[] args)
         {
+            int mode = Convert.ToInt32(Console.ReadLine());
+            Runtime.SetMode(RuntimeMode.TCP);
+            Runtime.IntroduceFronted(typeof(Server), typeof(Client));
+            if (mode == 0)
+            {
+                ServerMode();
+            }
+            else
+            {
+                ClientMode();
+            }
+
+            // Virtual mode
+            /*
             Runtime.SetMode(RuntimeMode.Virtual);
             Runtime.IntroduceFronted(typeof(Server), typeof(Client));
-          
+
             Dictionary<string, dynamic> map = new Dictionary<string, dynamic>();
             map.Add("heartBeatFrequency", 7);
             map.Add("listCount", 100000);
@@ -26,7 +65,7 @@ namespace AvgOfArray
 
             serverRuntime.RunTask();
 
-            Console.ReadLine();
+            Console.ReadLine();*/
         }
     }
 
---
+++ Examples/AvgOfArray/AvgOfArray/ResultLogCsv.cs
@@ -0,0 +1,26 @@
+﻿using CCMLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvgOfArray
+{
+    internal class ResultLogCsv : LoggerFront
+    {
+        private StringBuilder _stringBuilder = new StringBuilder();
+
+        public ResultLogCsv() {}
+
+        public override void Print(params dynamic[] values)
+        {
+            _stringBuilder.AppendLine($"{values[0]};{values[1]}");
+        }
+
+        public void SaveResults()
+        {
+            File.WriteAllText("results.csv", _stringBuilder.ToString());
+        }
+    }
+}
